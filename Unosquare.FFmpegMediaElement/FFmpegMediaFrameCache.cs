3a9dda2af884611ff9144ec22610e3cfd7ff35eb
Mario Di Vece <mario@unosquare.com>
2016-10-23 16:42:13 -0500

    Fixing issue #10 out of range exceptions because frame operations where not atomic.

--- Unosquare.FFmpegMediaElement/FFmpegMediaFrameCache.cs
+++ Unosquare.FFmpegMediaElement/FFmpegMediaFrameCache.cs
@@ -12,6 +12,8 @@
         private readonly List<FFmpegMediaFrame> Frames = new List<FFmpegMediaFrame>();
         private readonly IComparer<FFmpegMediaFrame> FrameStartTimeComparer = Comparer<FFmpegMediaFrame>.Create((a, b) => a.StartTime.CompareTo(b.StartTime));
 
+        private readonly object SyncLock = new object();
+
         #region Properties
 
         /// <summary>
@@ -82,8 +84,8 @@
         {
             get
             {
-                if (this.Count == 0) return null;
-                return this.Frames[this.MiddleIndex];
+                if (Count == 0) return null;
+                return Frames[MiddleIndex];
             }
         }
 
@@ -95,7 +97,7 @@
             get
             {
                 if (this.Count == 0) return null;
-                return this.Frames[0];
+                return Frames[0];
             }
         }
 
@@ -107,7 +109,7 @@
             get
             {
                 if (this.Count == 0) return null;
-                return this.Frames[this.Frames.Count - 1];
+                return Frames[Frames.Count - 1];
             }
         }
 
@@ -124,8 +126,8 @@
         public FFmpegMediaFrameCache(FFmpegMediaFrameCache otherCache)
         {
             var capacity = otherCache.Capacity;
-            this.Type = otherCache.Type;
-            this.Capacity = capacity;
+            Type = otherCache.Type;
+            Capacity = capacity;
             RecomputeProperties();
         }
 
@@ -138,8 +140,8 @@
         {
             if (frameRate <= 0) frameRate = 25;
             var capacity = (int)Math.Round(frameRate, 0) * 2;
-            this.Type = type;
-            this.Capacity = capacity;
+            Type = type;
+            Capacity = capacity;
             RecomputeProperties();
         }
 
@@ -152,16 +154,20 @@
         /// </summary>
         private void RecomputeProperties()
         {
-            this.Count = Frames.Count;
-            this.MiddleIndex = Frames.Count / 2;
-            this.FirstFrameTime = Frames.Count > 0 ? Frames[0].StartTime : 0M;
-            this.LastFrameTime = Frames.Count > 0 ? Frames[Frames.Count - 1].StartTime : 0M;
-            this.StartTime = this.FirstFrameTime > 0M ? this.FirstFrameTime : 0M;
-            this.EndTime = this.LastFrameTime > 0M ? this.LastFrameTime + LastFrame.Duration : 0M;
-            this.Duration = this.EndTime - this.StartTime;
-            this.IsFull = this.Count >= this.Capacity;
-            this.IsEmpty = this.Count <= 0;
-            this.MiddleFrameTime = Frames.Count > 0 ? this.MiddleFrame.StartTime : 0M;
+            lock (SyncLock)
+            {
+                Count = Frames.Count;
+                MiddleIndex = Frames.Count / 2;
+                FirstFrameTime = Frames.Count > 0 ? Frames[0].StartTime : 0M;
+                LastFrameTime = Frames.Count > 0 ? Frames[Frames.Count - 1].StartTime : 0M;
+                StartTime = FirstFrameTime > 0M ? FirstFrameTime : 0M;
+                EndTime = LastFrameTime > 0M ? LastFrameTime + LastFrame.Duration : 0M;
+                Duration = EndTime - StartTime;
+                IsFull = Count >= Capacity;
+                IsEmpty = this.Count <= 0;
+                MiddleFrameTime = Frames.Count > 0 ? MiddleFrame.StartTime : 0M;
+            }
+
         }
 
         /// <summary>
@@ -181,24 +187,31 @@
         /// <exception cref="System.IndexOutOfRangeException">Buffer does not support the capacity of new elements</exception>
         public void Replace(FFmpegMediaFrameCache newFrames)
         {
-
-            if (newFrames.Count > Capacity)
-                throw new IndexOutOfRangeException("Buffer does not support the capacity of new elements");
-
-            foreach (var frame in Frames)
-                frame.EnqueueRelease();
-
-            Frames.Clear();
-
-            foreach (var frame in newFrames.Frames)
+            lock (SyncLock)
             {
-                if (frame.Type != this.Type)
-                    this.ThrowInvalidFrameTypeException(frame.Type);
+                if (newFrames.Count > Capacity)
+                    throw new IndexOutOfRangeException("Buffer does not support the capacity of new elements");
 
-                Frames.Add(frame);
+                foreach (var frame in Frames)
+                    frame.EnqueueRelease();
+
+                Frames.Clear();
+
+                try
+                {
+                    foreach (var frame in newFrames.Frames)
+                    {
+                        if (frame.Type != this.Type)
+                            this.ThrowInvalidFrameTypeException(frame.Type);
+
+                        Frames.Add(frame);
+                    }
+                }
+                finally
+                {
+                    RecomputeProperties();
+                }
             }
-
-            RecomputeProperties();
 
         }
 
@@ -210,18 +223,26 @@
         /// <exception cref="System.IndexOutOfRangeException">Buffer is already at capacity.</exception>
         public void Add(FFmpegMediaFrame frame)
         {
-            if (Frames.Count >= Capacity)
-                throw new IndexOutOfRangeException("Buffer is already at capacity.");
+            lock (SyncLock)
+            {
+                if (Frames.Count >= Capacity)
+                    throw new IndexOutOfRangeException("Buffer is already at capacity.");
 
-            if (frame.Type != this.Type)
-                this.ThrowInvalidFrameTypeException(frame.Type);
+                if (frame.Type != Type)
+                    this.ThrowInvalidFrameTypeException(frame.Type);
 
-            Frames.Add(frame);
+                try
+                {
+                    Frames.Add(frame);
 
-            if (frame.StartTime < LastFrameTime)
-                Frames.Sort(FrameStartTimeComparer);
-
-            RecomputeProperties();
+                    if (frame.StartTime < LastFrameTime)
+                        Frames.Sort(FrameStartTimeComparer);
+                }
+                finally
+                {
+                    RecomputeProperties();
+                }
+            }
         }
 
         /// <summary>
@@ -229,14 +250,23 @@
         /// </summary>
         public void RemoveFirst()
         {
-            if (Frames.Count == 0) return;
+            lock (SyncLock)
+            {
+                if (Frames.Count == 0)
+                    return;
 
-            var index = 0;
-            var frame = Frames[index];
-            Frames.RemoveAt(index);
-            frame.EnqueueRelease();
-
-            RecomputeProperties();
+                try
+                {
+                    var index = 0;
+                    var frame = Frames[index];
+                    Frames.RemoveAt(index);
+                    frame.EnqueueRelease();
+                }
+                finally
+                {
+                    RecomputeProperties();
+                }
+            }
         }
 
         /// <summary>
@@ -244,14 +274,23 @@
         /// </summary>
         public void RemoveLast()
         {
-            if (Frames.Count == 0) return;
+            lock (SyncLock)
+            {
+                if (Frames.Count == 0)
+                    return;
 
-            var index = Frames.Count - 1;
-            var frame = Frames[index];
-            Frames.RemoveAt(index);
-            frame.EnqueueRelease();
-
-            RecomputeProperties();
+                try
+                {
+                    var index = Frames.Count - 1;
+                    var frame = Frames[index];
+                    Frames.RemoveAt(index);
+                    frame.EnqueueRelease();
+                }
+                finally
+                {
+                    RecomputeProperties();
+                }
+            }
         }
 
         /// <summary>
@@ -259,11 +298,20 @@
         /// </summary>
         public void Clear()
         {
-            foreach (var frame in Frames)
-                frame.EnqueueRelease();
+            lock (SyncLock)
+            {
+                try
+                {
+                    foreach (var frame in Frames)
+                        frame.EnqueueRelease();
 
-            Frames.Clear();
-            RecomputeProperties();
+                    Frames.Clear();
+                }
+                finally
+                {
+                    RecomputeProperties();
+                }
+            }
         }
 
         /// <summary>
@@ -274,43 +322,47 @@
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private FFmpegMediaFrame SearchFrame(decimal renderTime)
         {
-            var frameCount = Frames.Count;
-
-            // fast condition checking
-            if (frameCount <= 0) return null;
-            if (frameCount == 1) return Frames[0];
-
-            // variable setup
-            var lowIndex = 0;
-            var highIndex = frameCount - 1;
-            var midIndex = 1 + lowIndex + (highIndex - lowIndex) / 2;
-
-            // edge condition cheching
-            if (Frames[lowIndex].StartTime >= renderTime) return Frames[lowIndex];
-            if (Frames[highIndex].StartTime <= renderTime) return Frames[highIndex];
-
-            // First guess, very low cost, very fast
-            if (midIndex < highIndex && renderTime >= Frames[midIndex].StartTime && renderTime < Frames[midIndex + 1].StartTime)
-                return Frames[midIndex];
-
-            // binary search
-            while (highIndex - lowIndex > 1)
+            lock (SyncLock)
             {
-                midIndex = lowIndex + (highIndex - lowIndex) / 2;
-                if (renderTime < Frames[midIndex].StartTime)
-                    highIndex = midIndex;
-                else
-                    lowIndex = midIndex;
+                var frameCount = Frames.Count;
+
+                // fast condition checking
+                if (frameCount <= 0) return null;
+                if (frameCount == 1) return Frames[0];
+
+                // variable setup
+                var lowIndex = 0;
+                var highIndex = frameCount - 1;
+                var midIndex = 1 + lowIndex + (highIndex - lowIndex) / 2;
+
+                // edge condition cheching
+                if (Frames[lowIndex].StartTime >= renderTime) return Frames[lowIndex];
+                if (Frames[highIndex].StartTime <= renderTime) return Frames[highIndex];
+
+                // First guess, very low cost, very fast
+                if (midIndex < highIndex && renderTime >= Frames[midIndex].StartTime && renderTime < Frames[midIndex + 1].StartTime)
+                    return Frames[midIndex];
+
+                // binary search
+                while (highIndex - lowIndex > 1)
+                {
+                    midIndex = lowIndex + (highIndex - lowIndex) / 2;
+                    if (renderTime < Frames[midIndex].StartTime)
+                        highIndex = midIndex;
+                    else
+                        lowIndex = midIndex;
+                }
+
+                // linear search
+                for (var i = highIndex; i >= lowIndex; i--)
+                {
+                    if (Frames[i].StartTime <= renderTime)
+                        return Frames[i];
+                }
+
+                return null;
             }
 
-            // linear search
-            for (var i = highIndex; i >= lowIndex; i--)
-            {
-                if (Frames[i].StartTime <= renderTime)
-                    return Frames[i];
-            }
-
-            return null;
         }
 
         /// <summary>
@@ -320,10 +372,14 @@
         /// <returns></returns>
         public FFmpegMediaFrame GetFrameAt(int index)
         {
-            if (index < Frames.Count && index >= 0)
-                return Frames[index];
-            else
-                return null;
+            lock (SyncLock)
+            {
+                if (index < Frames.Count && index >= 0)
+                    return Frames[index];
+                else
+                    return null;
+            }
+
         }
 
         /// <summary>
@@ -333,13 +389,17 @@
         /// <returns></returns>
         public FFmpegMediaFrame GetFrame(decimal renderTime, bool checkBounds)
         {
-            if (Frames.Count <= 0) return null;
-            if (checkBounds && renderTime < this.StartTime) return null;
-            if (checkBounds && renderTime > this.EndTime) return null;
+            lock (SyncLock)
+            {
+                if (Frames.Count <= 0) return null;
+                if (checkBounds && renderTime < this.StartTime) return null;
+                if (checkBounds && renderTime > this.EndTime) return null;
 
-            if (renderTime < Frames[0].StartTime) return Frames[0];
+                if (renderTime < Frames[0].StartTime) return Frames[0];
 
-            return SearchFrame(renderTime);
+                return SearchFrame(renderTime);
+            }
+
         }
 
         /// <summary>
@@ -350,19 +410,23 @@
         /// <returns></returns>
         public List<FFmpegMediaFrame> GetFrames(decimal renderTime, int frameCount)
         {
-            var result = new List<FFmpegMediaFrame>();
-            var startFrameIndex = IndexOf(renderTime, false);
-            if (startFrameIndex < 0) return result;
-
-            for (var i = startFrameIndex; i < Frames.Count; i++)
+            lock (SyncLock)
             {
-                if (result.Count >= frameCount)
-                    break;
-                else
-                    result.Add(Frames[i]);
+                var result = new List<FFmpegMediaFrame>();
+                var startFrameIndex = IndexOf(renderTime, false);
+                if (startFrameIndex < 0) return result;
+
+                for (var i = startFrameIndex; i < Frames.Count; i++)
+                {
+                    if (result.Count >= frameCount)
+                        break;
+                    else
+                        result.Add(Frames[i]);
+                }
+
+                return result;
             }
 
-            return result;
         }
 
         /// <summary>
@@ -373,20 +437,24 @@
         /// <returns></returns>
         public List<FFmpegMediaFrame> GetFrames(decimal renderTime, decimal duration)
         {
-            var result = new List<FFmpegMediaFrame>();
-            var startFrameIndex = IndexOf(renderTime, false);
-            if (startFrameIndex < 0) return result;
-            var endTime = renderTime + duration;
-
-            for (var i = startFrameIndex; i < Frames.Count; i++)
+            lock (SyncLock)
             {
-                if (Frames[i].StartTime <= endTime)
-                    result.Add(Frames[i]);
-                else
-                    break;
+                var result = new List<FFmpegMediaFrame>();
+                var startFrameIndex = IndexOf(renderTime, false);
+                if (startFrameIndex < 0) return result;
+                var endTime = renderTime + duration;
+
+                for (var i = startFrameIndex; i < Frames.Count; i++)
+                {
+                    if (Frames[i].StartTime <= endTime)
+                        result.Add(Frames[i]);
+                    else
+                        break;
+                }
+
+                return result;
             }
 
-            return result;
         }
 
         /// <summary>
@@ -396,10 +464,14 @@
         /// <returns></returns>
         public int IndexOf(FFmpegMediaFrame frame)
         {
-            if (frame == null)
-                return -1;
+            lock (SyncLock)
+            {
+                if (frame == null)
+                    return -1;
 
-            return Frames.IndexOf(frame);
+                return Frames.IndexOf(frame);
+            }
+
         }
 
         /// <summary>
@@ -410,9 +482,13 @@
         /// <returns></returns>
         public int IndexOf(decimal renderTime, bool checkBounds)
         {
-            var frame = GetFrame(renderTime, checkBounds);
-            if (frame == null) return -1;
-            return Frames.IndexOf(frame);
+            lock (SyncLock)
+            {
+                var frame = GetFrame(renderTime, checkBounds);
+                if (frame == null) return -1;
+                return Frames.IndexOf(frame);
+            }
+
         }
 
         #endregion
