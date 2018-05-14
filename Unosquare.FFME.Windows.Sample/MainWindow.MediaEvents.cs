2c272b906494c3f478a399a16d84ec7be0a21886
Mario Di Vece <mario@unosquare.com>
2018-05-13 19:32:49 -0500

    minor bugfix

--- Unosquare.FFME.Windows.Sample/MainWindow.MediaEvents.cs
+++ Unosquare.FFME.Windows.Sample/MainWindow.MediaEvents.cs
@@ -162,13 +162,13 @@
                 // The yadif filter deinterlaces the video; we check the field order if we need
                 // to deinterlace the video automatically
                 if (requiresDeinterlace)
-                    videoFilter.Append("yadif;");
+                    videoFilter.Append("yadif,");
 
                 // Scale down to maximum 1080p screen resolution.
                 if (videoStream.PixelHeight > 1080)
-                    videoFilter.Append($"scale=-1:1080;");
+                    videoFilter.Append($"scale=-1:1080,");
 
-                e.Options.VideoFilter = videoFilter.ToString().TrimEnd(';');
+                e.Options.VideoFilter = videoFilter.ToString().TrimEnd(',');
             }
 
             // e.Options.AudioFilter = "aecho=0.8:0.9:1000:0.3";
