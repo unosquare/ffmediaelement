48f79a10d119c1d92c3a5a44d77cd2cff42d0d66
Mario Di Vece <mario@unosquare.com>
2018-06-03 11:06:59 -0500

    trailing whitespace - duh.

--- Unosquare.FFME.Windows.Sample/ViewModels/RootViewModel.cs
+++ Unosquare.FFME.Windows.Sample/ViewModels/RootViewModel.cs
@@ -161,7 +161,7 @@
                 state = "Ready";
             }
 
-            WindowTitle = $"{title} - {state} - Unosquare FFME Play v{AppVersion} " 
+            WindowTitle = $"{title} - {state} - Unosquare FFME Play v{AppVersion} "
                 + $"FFmpeg {MediaElement.FFmpegVersionInfo} ({(GuiContext.Current.IsInDebugMode ? "Debug" : "Release")})";
         }
     }
