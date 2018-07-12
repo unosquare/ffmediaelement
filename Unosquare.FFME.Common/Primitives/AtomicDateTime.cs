d78d3e31d0966fa5fe2c475db1146a1df5003076
Mario Di Vece <mario@unosquare.com>
2018-07-11 22:09:33 -0500

    adding missing atomic types

---
+++ Unosquare.FFME.Common/Primitives/AtomicDateTime.cs
@@ -0,0 +1,38 @@
+﻿namespace Unosquare.FFME.Primitives
+{
+    using System;
+
+    /// <summary>
+    /// Defines an atomic DateTime
+    /// </summary>
+    public sealed class AtomicDateTime : AtomicTypeBase<DateTime>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AtomicDateTime"/> class.
+        /// </summary>
+        /// <param name="initialValue">The initial value.</param>
+        public AtomicDateTime(DateTime initialValue)
+            : base(initialValue.Ticks)
+        {
+            // placeholder
+        }
+
+        /// <summary>
+        /// Converts froma long value to the target type.
+        /// </summary>
+        /// <param name="backingValue">The backing value.</param>
+        /// <returns>
+        /// The value converted form a long value
+        /// </returns>
+        protected override DateTime FromLong(long backingValue) => new DateTime(backingValue);
+
+        /// <summary>
+        /// Converts from the target type to a long value
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The value converted to a long value
+        /// </returns>
+        protected override long ToLong(DateTime value) => value.Ticks;
+    }
+}
---
+++ Unosquare.FFME.Common/Primitives/AtomicTimeSpan.cs
@@ -0,0 +1,38 @@
+﻿namespace Unosquare.FFME.Primitives
+{
+    using System;
+
+    /// <summary>
+    /// Represents an atomic TimeSpan type
+    /// </summary>
+    public sealed class AtomicTimeSpan : AtomicTypeBase<TimeSpan>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AtomicTimeSpan" /> class.
+        /// </summary>
+        /// <param name="initialValue">The initial value.</param>
+        public AtomicTimeSpan(TimeSpan initialValue)
+                    : base(initialValue.Ticks)
+        {
+            // placeholder
+        }
+
+        /// <summary>
+        /// Converts froma long value to the target type.
+        /// </summary>
+        /// <param name="backingValue">The backing value.</param>
+        /// <returns>
+        /// The value converted form a long value
+        /// </returns>
+        protected override TimeSpan FromLong(long backingValue) => TimeSpan.FromTicks(backingValue);
+
+        /// <summary>
+        /// Converts from the target type to a long value
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The value converted to a long value
+        /// </returns>
+        protected override long ToLong(TimeSpan value) => value.Ticks;
+    }
+}
