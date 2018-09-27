6c72b9b891145d035ab4b6a0dd36b9d335a3d772
xj42 <29698861+xj42@users.noreply.github.com>
2018-09-27 23:10:57 +0900

    Calculating subtitle end time (#285)
    * Calculating subtitle end time
    
    This gives the proper TimeSpan for the EndTime.
    
    according to the ffmpeg docs:
     3875 typedef struct AVSubtitle {
     3876     uint16_t format; /* 0 = graphics */
     3877     uint32_t start_display_time; /* relative to packet pts, in ms */
     3878     uint32_t end_display_time; /* relative to packet pts, in ms */
     3879     unsigned num_rects;
     3880     AVSubtitleRect **rects;
     3881     int64_t pts;    ///< Same as packet pts, in AV_TIME_BASE
     3882 } AVSubtitle;
    
    * Correction
    
    * blank line

--- Unosquare.FFME.Common/Decoding/SubtitleFrame.cs
+++ Unosquare.FFME.Common/Decoding/SubtitleFrame.cs
@@ -37,9 +37,9 @@
             var timeOffset = TimeSpan.FromTicks(frame->pts.ToTimeSpan(ffmpeg.AV_TIME_BASE).Ticks - mainOffset.Ticks);
 
             // start_display_time and end_display_time are relative to timeOffset
-            StartTime = TimeSpan.FromTicks(timeOffset.Ticks + Convert.ToInt64(frame->start_display_time).ToTimeSpan(StreamTimeBase).Ticks);
-            EndTime = TimeSpan.FromTicks(timeOffset.Ticks + Convert.ToInt64(frame->end_display_time).ToTimeSpan(StreamTimeBase).Ticks);
-            Duration = TimeSpan.FromTicks(EndTime.Ticks - StartTime.Ticks);
+            StartTime = TimeSpan.FromMilliseconds(timeOffset.TotalMilliseconds + frame->start_display_time);
+            EndTime = TimeSpan.FromMilliseconds(timeOffset.TotalMilliseconds + frame->end_display_time);
+            Duration = TimeSpan.FromMilliseconds(frame->end_display_time - frame->start_display_time);
 
             // Extract text strings
             TextType = AVSubtitleType.SUBTITLE_NONE;
