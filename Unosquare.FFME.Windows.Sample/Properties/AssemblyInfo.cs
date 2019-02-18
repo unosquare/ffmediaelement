042155015496ce6355d7a08d3f4854a5351693c8
Mario Di Vece <mario@unosquare.com>
2019-02-18 01:21:04 -0600

    bumping up assembly versions

--- Unosquare.FFME.Windows.Sample/Properties/AssemblyInfo.cs
+++ Unosquare.FFME.Windows.Sample/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.01.21.*")]
+[assembly: AssemblyVersion("2019.02.18.*")]
 [assembly: AssemblyFileVersion("4.1.280")]
