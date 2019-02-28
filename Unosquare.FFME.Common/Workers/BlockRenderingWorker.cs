a2c4eeba7058ee1e90b8ec29ae815ce097bbbebf
Mario Di Vece <mario@unosquare.com>
2019-02-28 11:45:32 -0600

    splitting block rendering logic into individual functions to ensure more clarity

--- Unosquare.FFME.Common/Workers/BlockRenderingWorker.cs
+++ Unosquare.FFME.Common/Workers/BlockRenderingWorker.cs
@@ -53,186 +53,58 @@
         /// </summary>
         private MediaEngineState State { get; }
 
+        /// <summary>
+        /// Whether or not blocks should be sent to their renderers in parallel.
+        /// </summary>
+        private bool UseParallelRendering { get; }
+
         /// <inheritdoc />
         protected override void ExecuteCycleLogic(CancellationToken ct)
         {
             // Update Status Properties
             var main = Container.Components.MainMediaType;
             var all = MediaCore.Renderers.Keys.ToArray();
-            var playbackClock = MediaCore.PlaybackClock;
-
-            if (HasInitialized == false)
-            {
-                // wait for main component blocks or EOF or cancellation pending
-                if (MediaCore.Blocks[main].Count <= 0)
-                    return;
-
-                var startTime = MediaCore.Blocks[main].RangeStartTime;
-
-                // Set the initial clock position
-                MediaCore.State.UpdatePlaybackStartTime(startTime);
-                MediaCore.ChangePlaybackPosition(startTime);
-
-                // Wait for renderers to be ready
-                foreach (var t in all)
-                    MediaCore.Renderers[t]?.WaitForReadyState();
-
-                // Mark as initialized
-                HasInitialized.Value = true;
-            }
-
-            #region Run the Rendering Cycle
 
             try
             {
-                #region 0. Clock Control Logic
+                // Ensure we have renderers ready and main blocks available
+                if (!Initialize(main, all)) return;
 
-                // Control the clock based on
-                if (Commands.IsSeeking == false)
-                {
-                    var blocks = MediaCore.Blocks[main];
-                    var range = blocks.GetRangePercent(playbackClock);
+                // Ensure we have sufficient blocks on all components
+                // and the clock is in the proper state
+                if (!EnsureBlocksAvailable(main, all)) return;
 
-                    if (range >= 1d)
-                    {
-                        // Don't let the RTC move beyond what is available on the main component
-                        MediaCore.Clock.Pause();
-                        MediaCore.ChangePlaybackPosition(blocks.RangeEndTime);
-                    }
-                    else if (range < 0)
-                    {
-                        // Don't let the RTC lag behind what is available on the main component
-                        MediaCore.ChangePlaybackPosition(blocks.RangeStartTime);
-                    }
-
-                    // TODO: This is anew approach to sync-buffering
-                    if (MediaCore.ShouldReadMorePackets)
-                    {
-                        foreach (var t in all)
-                        {
-                            if (t == main || t == MediaType.Subtitle)
-                                continue;
-
-                            range = MediaCore.Blocks[t].GetRangePercent(playbackClock);
-
-                            if (State.BufferingProgress < 1 && (range < 0 || range >= 1))
-                            {
-                                MediaCore.Clock.Pause();
-                                return;
-                            }
-                        }
-                    }
-
-                    // always ensure we run the clock if we need a play status
-                    // TODO the rendering worker must always control the clock
-                    if (MediaCore.State.MediaState == PlaybackStatus.Play)
-                        MediaCore.Clock.Play();
-                }
-
-                #endregion
-
-                #region 1. Wait for any seek operation to make blocks available in a loop
-
-                while (!ct.IsCancellationRequested
-                && Commands.IsActivelySeeking
-                && !MediaCore.Blocks[main].IsInRange(MediaCore.PlaybackClock))
-                {
-                    // Check if we finally have seek blocks available
-                    // if we don't get seek blocks in range and we are not step-seeking,
-                    // then we simply break out of the loop and render whatever it is we have
-                    // to create the illussion of smooth seeking. For precision seeking we
-                    // continue the loop.
-                    if (!Commands.WaitForSeekBlocks(DefaultPeriod.Milliseconds)
-                        && Commands.ActiveSeekMode == CommandManager.SeekMode.Normal)
-                    {
-                        break;
-                    }
-                }
-
-                #endregion
-
-                #region 2. Handle Block Rendering
-
-                // Capture the blocks to render at a fixed wall clock position
-                // so all blocks are aligned to the same timestamp
-                playbackClock = MediaCore.PlaybackClock;
+                // If we are in the middle of a seek, wait for seek blocks
+                WaitForSeekBlocks(main, ct);
 
                 // Render each of the Media Types if it is time to do so.
-                Parallel.ForEach(all, (t) =>
+                if (UseParallelRendering)
                 {
-                    try
-                    {
-                        // We don't need non-video blocks if we are seeking
-                        if (Commands.IsSeeking && t != MediaType.Video)
-                            return;
-
-                        // Get the audio, video, or subtitle block to render
-                        var currentBlock = t == MediaType.Subtitle && MediaCore.PreloadedSubtitles != null
-                            ? MediaCore.PreloadedSubtitles[playbackClock]
-                            : MediaCore.Blocks[t][playbackClock];
-
-                        // Don't send null or disposed blocks to renderer
-                        if (currentBlock == null || currentBlock.IsDisposed)
-                            return;
-
-                        // Render by forced signal (TimeSpan.MinValue) or because simply it is time to do so
-                        var lastBlockStartTime = MediaCore.LastRenderTime[t];
-                        if (lastBlockStartTime != currentBlock.StartTime || lastBlockStartTime == TimeSpan.MinValue)
-                            SendBlockToRenderer(currentBlock, playbackClock);
-                    }
-                    finally
-                    {
-                        // Call the update method on all renderers so they receive what the new playback clock is.
-                        MediaCore.Renderers[t]?.Update(playbackClock);
-                    }
-                });
-
-                #endregion
+                    Parallel.ForEach(all, (t) =>
+                        RenderBlock(t));
+                }
+                else
+                {
+                    foreach (var t in all)
+                        RenderBlock(t);
+                }
             }
             catch (Exception ex)
             {
-                MediaCore.LogError(Aspects.RenderingWorker, "Error while in rendering worker cycle", ex);
+                MediaCore.LogError(
+                    Aspects.RenderingWorker, "Error while in rendering worker cycle", ex);
+
                 throw;
             }
             finally
             {
-                var playbackEndClock = MediaCore.Blocks[main].Count > 0
-                    ? MediaCore.Blocks[main].RangeEndTime
-                    : Container.Components.PlaybackEndTime ?? TimeSpan.MaxValue;
-
-                // Check End of Media Scenarios
-                if (Commands.IsSeeking == false
-                && MediaCore.HasDecodingEnded
-                && MediaCore.PlaybackClock.Ticks >= playbackEndClock.Ticks)
-                {
-                    // Rendered all and nothing else to render
-                    if (State.HasMediaEnded == false)
-                    {
-                        MediaCore.Clock.Pause();
-
-                        // TODO: compute end position
-                        // var endPosition = TimeSpan.FromTicks(MediaCore.LastRenderTime[main].Ticks + RenderDuration[main].Ticks);
-                        // endPosition = MediaCore.ChangePosition(endPosition);
-                        // State.UpdateMediaEnded(true, endPosition);
-                        // TODO: The below needs adding the last block durration
-                        var endPosition = MediaCore.ChangePlaybackPosition(playbackEndClock);
-                        State.UpdateMediaEnded(true, endPosition);
-
-                        State.UpdateMediaState(PlaybackStatus.Stop);
-                        MediaCore.InvalidateRenderers();
-                    }
-                }
-                else
-                {
-                    State.UpdateMediaEnded(false, TimeSpan.Zero);
-                }
+                // Detect end of media scenarios
+                DetectHasMediaEnded(main);
 
                 // Update the Position
-                if (!ct.IsCancellationRequested && Commands.IsSeeking == false)
+                if (Commands.IsSeeking == false)
                     State.ReportPlaybackPosition();
             }
-
-            #endregion
         }
 
         /// <inheritdoc />
@@ -245,33 +117,195 @@
             // nothing needed when disposing
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool Initialize(MediaType main, MediaType[] all)
+        {
+            if (HasInitialized == true) return true;
+
+            // wait for main component blocks or EOF or cancellation pending
+            if (MediaCore.Blocks[main].Count <= 0)
+                return false;
+
+            var startTime = MediaCore.Blocks[main].RangeStartTime;
+
+            // Set the initial clock position
+            MediaCore.State.UpdatePlaybackStartTime(startTime);
+            MediaCore.ChangePlaybackPosition(startTime);
+
+            // Wait for renderers to be ready
+            foreach (var t in all)
+                MediaCore.Renderers[t]?.WaitForReadyState();
+
+            // Mark as initialized
+            HasInitialized.Value = true;
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool EnsureBlocksAvailable(MediaType main, MediaType[] all)
+        {
+            if (Commands.IsSeeking)
+                return true;
+
+            var blocks = MediaCore.Blocks[main];
+            var range = blocks.GetRangePercent(MediaCore.PlaybackClock);
+
+            if (range >= 1d)
+            {
+                // Don't let the RTC move beyond what is available on the main component
+                MediaCore.Clock.Pause();
+                MediaCore.ChangePlaybackPosition(blocks.RangeEndTime);
+            }
+            else if (range < 0)
+            {
+                // Don't let the RTC lag behind what is available on the main component
+                MediaCore.ChangePlaybackPosition(blocks.RangeStartTime);
+            }
+
+            // TODO: This is anew approach to sync-buffering
+            // it still needs work -- see prior sync-buffering logic
+            if (MediaCore.ShouldReadMorePackets)
+            {
+                foreach (var t in all)
+                {
+                    if (t == main || t == MediaType.Subtitle)
+                        continue;
+
+                    range = MediaCore.Blocks[t].GetRangePercent(MediaCore.PlaybackClock);
+
+                    if (State.BufferingProgress < 1 && (range <= 0 || range >= 1))
+                    {
+                        MediaCore.Clock.Pause();
+                        return false;
+                    }
+                }
+            }
+
+            // always ensure we run the clock if we need a play status
+            // TODO: the rendering worker must always control the clock -- find where this is not the case
+            // all references to updating the clock or resuming playback must be removed
+            if (MediaCore.State.MediaState == PlaybackStatus.Play)
+                MediaCore.Clock.Play();
+
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void WaitForSeekBlocks(MediaType main, CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested
+            && Commands.IsActivelySeeking
+            && !MediaCore.Blocks[main].IsInRange(MediaCore.PlaybackClock))
+            {
+                // Check if we finally have seek blocks available
+                // if we don't get seek blocks in range and we are not step-seeking,
+                // then we simply break out of the loop and render whatever it is we have
+                // to create the illussion of smooth seeking. For precision seeking we
+                // continue the loop.
+                if (Commands.ActiveSeekMode == CommandManager.SeekMode.Normal
+                && !Commands.WaitForSeekBlocks(DefaultPeriod.Milliseconds))
+                {
+                    break;
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool RenderBlock(MediaType t)
+        {
+            var result = 0;
+            var playbackClock = MediaCore.PlaybackClock;
+
+            try
+            {
+                // We don't need non-video blocks if we are seeking
+                if (Commands.IsSeeking && t != MediaType.Video)
+                    return result > 0;
+
+                // Get the audio, video, or subtitle block to render
+                var currentBlock = t == MediaType.Subtitle && MediaCore.PreloadedSubtitles != null
+                    ? MediaCore.PreloadedSubtitles[playbackClock]
+                    : MediaCore.Blocks[t][playbackClock];
+
+                // Send the block to the corresponding renderer
+                // this will handle fringe and skip cases
+                result += SendBlockToRenderer(currentBlock, playbackClock);
+            }
+            finally
+            {
+                // Call the update method on all renderers so they receive what the new playback clock is.
+                MediaCore.Renderers[t]?.Update(playbackClock);
+            }
+
+            return result > 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void DetectHasMediaEnded(MediaType main)
+        {
+            var playbackEndClock = MediaCore.Blocks[main].Count > 0
+                ? MediaCore.Blocks[main].RangeEndTime
+                : Container.Components.PlaybackEndTime ?? TimeSpan.MaxValue;
+
+            // Check End of Media Scenarios
+            if (Commands.IsSeeking == false
+                && MediaCore.HasDecodingEnded
+                && MediaCore.PlaybackClock.Ticks >= playbackEndClock.Ticks)
+            {
+                // Rendered all and nothing else to render
+                if (State.HasMediaEnded == false)
+                {
+                    MediaCore.Clock.Pause();
+
+                    // TODO: compute end update end position
+                    // this will get overwritten when changemedia gets called
+                    // maybe we need to save it separately
+                    var endPosition = MediaCore.ChangePlaybackPosition(playbackEndClock);
+                    State.UpdateMediaEnded(true, endPosition);
+
+                    State.UpdateMediaState(PlaybackStatus.Stop);
+                    MediaCore.InvalidateRenderers();
+                }
+            }
+            else
+            {
+                State.UpdateMediaEnded(false, TimeSpan.Zero);
+            }
+        }
+
         /// <summary>
         /// Sends the given block to its corresponding media renderer.
         /// </summary>
-        /// <param name="block">The block.</param>
+        /// <param name="currentBlock">The block.</param>
         /// <param name="playbackPosition">The clock position.</param>
         /// <returns>
         /// The number of blocks sent to the renderer
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private int SendBlockToRenderer(MediaBlock block, TimeSpan playbackPosition)
+        private int SendBlockToRenderer(MediaBlock currentBlock, TimeSpan playbackPosition)
         {
             // No blocks were rendered
-            if (block == null) return 0;
+            if (currentBlock == null || currentBlock.IsDisposed) return 0;
 
-            var t = block.MediaType;
+            var t = currentBlock.MediaType;
+            var lastBlockStartTime = MediaCore.LastRenderTime[t];
+
+            // Render by forced signal (TimeSpan.MinValue) or because simply it is time to do so
+            // otherwise simply skip block rendering as we have sent the block already.
+            if (lastBlockStartTime != TimeSpan.MinValue && lastBlockStartTime == currentBlock.StartTime)
+                return 0;
 
             // Process property changes coming from video blocks
-            State.UpdateDynamicBlockProperties(block, MediaCore.Blocks[t]);
+            State.UpdateDynamicBlockProperties(currentBlock, MediaCore.Blocks[t]);
 
             // Capture the last render time so we don't repeat the block
-            MediaCore.LastRenderTime[t] = block.StartTime;
+            MediaCore.LastRenderTime[t] = currentBlock.StartTime;
 
             // Send the block to its corresponding renderer
-            MediaCore.Renderers[t]?.Render(block, playbackPosition);
+            MediaCore.Renderers[t]?.Render(currentBlock, playbackPosition);
 
             // Log the block statistics for debugging
-            LogRenderBlock(block, playbackPosition);
+            LogRenderBlock(currentBlock, playbackPosition);
 
             return 1;
         }
@@ -291,7 +325,7 @@
             try
             {
                 var drift = TimeSpan.FromTicks(clockPosition.Ticks - block.StartTime.Ticks);
-                this.LogInfo(Aspects.RenderingWorker,
+                this.LogTrace(Aspects.RenderingWorker,
                     $"{block.MediaType.ToString().Substring(0, 1)} "
                     + $"BLK: {block.StartTime.Format()} | "
                     + $"CLK: {clockPosition.Format()} | "
