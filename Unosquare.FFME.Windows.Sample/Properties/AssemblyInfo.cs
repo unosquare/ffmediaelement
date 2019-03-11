ef2d4d7f8245d4015d27fee1ea19edb92867cd70
Mario Di Vece <mario@unosquare.com>
2019-03-10 20:53:50 -0600

    bumping up version date. Restoring default ffmpeg search path.

--- Unosquare.FFME.Windows.Sample/Properties/AssemblyInfo.cs
+++ Unosquare.FFME.Windows.Sample/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.02.18.*")]
+[assembly: AssemblyVersion("2019.03.10.*")]
 [assembly: AssemblyFileVersion("4.1.280")]
