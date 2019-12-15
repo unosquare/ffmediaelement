7903074d6655ed552952ad3e2fb27a1746ce4c1a
Mario Di Vece <mario@unosquare.com>
2019-12-14 19:21:07 -0600

    removing D3DImage and adding VideoImageType option as a RendererOption

--- Unosquare.FFME.Windows/Platform/MediaConnector.cs
+++ Unosquare.FFME.Windows/Platform/MediaConnector.cs
@@ -15,9 +15,12 @@
                 case MediaType.Audio:
                     return new AudioRenderer(mediaCore);
                 case MediaType.Video:
-                    return D3DVideoRenderer.IsAvailable
-                        ? new InteropVideoRenderer(mediaCore) as IMediaRenderer
-                        : new VideoRenderer(mediaCore);
+                    return ((mediaCore.Parent as MediaElement)?.RendererOptions.VideoImageType ?? VideoRendererImageType.WriteableBitmap) switch
+                    {
+                        VideoRendererImageType.WriteableBitmap => new VideoRenderer(mediaCore),
+                        VideoRendererImageType.InteropBitmap => new InteropVideoRenderer(mediaCore),
+                        _ => new VideoRenderer(mediaCore),
+                    };
                 case MediaType.Subtitle:
                     return new SubtitleRenderer(mediaCore);
                 default:
